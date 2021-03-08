2d17c842aab69b7843635f26a8d2180acf7d225d
kgs <kgs@gmail.com>
2021-03-08 18:16:44 +0800

    ~~~

--- resources/views/shipManage/equipment_type_manage.blade.php
+++ resources/views/shipManage/equipment_type_manage.blade.php
@@ -132,7 +132,7 @@ $shipList = Session::get('shipList');
                             <h4 class="lighter smaller col-md-4" style="color: #ffffff">{{ transShipManager('title.equipment_unit_table') }}</h4>
                             @if(!$isHolder)
                                 <div class="widget-toolbar action-buttons col-md-3">
-                                    <button class="btn btn-primary new_sub_btn" id="id-btn-adddialog" style="width: 100%">
+                                    <button class="btn btn-primary new_sub_btn" id="id-btn-adddialog">
                                         <i class="icon-plus-sign-alt"></i>
                                         {{ trans('common.label.add') }}
                                     </button>
