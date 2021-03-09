196c5602da73b56a92d699e7762f521a24226b00
pkh <pkh@jxgi.com>
2021-03-09 09:34:11 +0800

    menu

---
+++ config/fndatabase.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017-10-28
+ * Time: 오후 9:56
+ */
+
+set_include_path(app_path() . '/Models/Home' . PATH_SEPARATOR . get_include_path());
+require_once 'Zend/Db.php';
+
+$mysqlOptions = array(
+    'host' => 'localhost',
+    'dbname' => 'kyongsong'.'2018'/*date("20y")*/,
+    'username' => 'test',
+    'password' => 'mima123456789',
+    'port' => 3306,
+    'charset' => 'UTF8',
+    'persistent' => false
+);
+
+global $g_dbAdapter;
+$g_dbAdapter = Zend_Db::factory('pdo_mysql', $mysqlOptions);
