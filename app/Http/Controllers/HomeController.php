9ef4933e3f522ee0615c298413423cf409d0f09d
kgs <kgs@gmail.com>
2021-05-26 12:17:27 +0900

    ---

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -60,22 +60,11 @@ class HomeController extends Controller {
 		foreach($reportList as $key => $item) {
 			$reportList[$key]->realname = UserInfo::find($item->creator)['realname'];
 		}
-		$shipList = ShipRegister::getShipListByOrigin();
-		$tmp = ShipRegister::getSimpleDataList();
 		$shipList = ShipRegister::all();
 		$shipForDecision = array();
-		foreach($tmp as $key => $item) {
-			$shipForDecision[$item->id] = $item->shipName_Cn;
 		foreach($shipList as $key => $item) {
 			$shipForDecision[$item->IMO_No] = $item->shipName_En;
 		}
-
-
-
-
-
-
-
 
 		$expired_certData['ship'] = array();
 		$expired_certData['member'] = array();
@@ -99,153 +88,6 @@ class HomeController extends Controller {
 		]);
 	}
 
-	public function file_download(Request $request) {
-
-		$filepath = $request->get('path');
-		$type = $request->get('type');
-		$path = public_path('uploads');
-		$filename = $request->get('filename');
-
-
-		if($type == 'news')
-			$path = $path.'/news/'.$filepath;
-
-		else if($type == 'report')
-			$path = $path.'/report/'.$filepath;
-
-		else if($type == 'training')
-			$path = $path.'/training/'.$filepath;
-
-		else if($type == 'crewCard')
-			$path = $path.'/crewCard/'.$filepath;
-
-		else if($type == 'capacity')
-			$path = $path.'/capacity/'.$filepath;
-
-		else if($type == 'school')
-			$path = $path.'/school/'.$filepath;
-		else if($type == 'ship-cert')
-			$path = $path.'/ship-cert/'.$filepath;
-        else if($type == 'repair')
-            $path = $path.'/repair/'.$filepath;
-
-		return response()->download($path, $filename);
-	}
-
-	public function translatePage($locale) {
-
-        if (!in_array($locale, Config::get('app.locales')))
-            $locale = Config::get('app.locale');
-
-        Session::put('locale', $locale);
-        App::setLocale($locale);
-
-        return redirect()->back();
-    }
-
-
-	public function testpage() {
-
-		return view('operation.supply.icons');
-	}
-
-    //오늘이 공작일인가를 검사한다.
-    private function isWorkingDay($selDate = null)
-    {
-        if(is_null($selDate))
-            $selDate = date("Y-m-d");
-        $restList = AttendRest::where('day', $selDate)->get();
-        if(count($restList) > 0)
-            return false;
-
-        return true;
-    }
-
-    //현재시간이 로동시간에 포함되는가를 검사한다.//지각생판정
-    public function isValidateOfTime()
-    {
-        $id = AttendTime::max('id');
-        $attendtime = AttendTime::find($id);
-        $attend = date('Y-m-d').' '. $attendtime->start;
-
-        $time1 = date_timestamp_get(date_create());
-        $time2 = date_timestamp_get(date_create($attend));
-
-        if ( $time1 < $time2) {
-            return true;
-        }
-        return false;
-    }
-
-    //ID목록으로从 이름목록을 얻기
-    private function getUserNames($idList)
-    {
-        $idList = explode(',', $idList);
-        $count = 0;
-        foreach ($idList as $userid) {
-            if(empty($userid))
-                continue;
-            $count++;
-        }
-
-        $user = Auth::user();
-
-        $namestr = $user->name;
-        if($count > 1)
-            $namestr .=  '외'.($count - 1).'명';
-        return $namestr;
-    }
-
-    public function resetPassword(Request $request) {
-        $old_passwd = $request->get('old_passwd');
-        $new_passwd = $request->get('password');
-        $confirm_passwd = $request->get('password_confirmation');
-
-        $state = Session::get('state');
-        $msg = Session::get('msg');
-
-        if(empty($new_passwd))
-            return view('auth.reset', ['state'=>$state, 'msg'=>$msg]);
-
-        if($new_passwd != $confirm_passwd) {
-            $msg = "암호를 다시 정확히 입력하십시오.";
-            return back()->with(['state'=>'error','msg'=>$msg]);
-        }
-
-        $user = Auth::user();
-        $password = $user->password;
-
-        if( password_verify($old_passwd, $password)){
-            $user['password'] = Hash::make($new_passwd);
-            $user->save();
-            return redirect('/home');
-        } else {
-            $msg = "이전의 암호가 정확치 않습니다. 다시 입력하십시오.";
-            return back()->with(['state'=>'error','msg'=>$msg]);
-        }
-    }
-
-	//은행시세표 搜索
-	public function searchList(Request $request){
-
-		$RateModel = new Home\RateModel();
-        $year = $request->get('year');
-	    $month = $request->get('month');
-        print_r($month);die;
-
-//        $searchData = $RateModel->getSearchRate($year, $month);
-//		//print_r($searchData);
-//		//	die("dsf");
-//		$jsonResultData = json_encode($searchData);
-//		print_r($jsonResultData);die();
-
-
-
-	}
-
-	public function gotoWelcome() {
-        return view('home.welcome');
-    }
 	public function resetPassword(Request $request) {
         $old_passwd = $request->get('old_passwd');
         $new_passwd = $request->get('password');
@@ -275,5 +117,6 @@ class HomeController extends Controller {
         }
     }
 }
+
 
 
--- resources/views/home/front.blade.php
+++ resources/views/home/front.blade.php
@@ -1,9 +1,7 @@
 @extends('layout.header')
 
-@section('sidebar')
 @section('content')
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <link href="{{ cAsset('assets/css/slides.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ cAsset('assets/js/chartjs/chartist.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ cAsset('assets/js/chartjs/c3.css') }}" rel="stylesheet" type="text/css">
@@ -14,9 +12,6 @@
     <script src="{{ cAsset('assets/js/chartjs/d3.js') }}"></script>
     <script src="{{ cAsset('assets/js/chartjs/c3.js') }}"></script>
     <script src="{{ cAsset('assets/js/chartjs/chartjs.js') }}"></script>
-    <script src="{{ cAsset('js/dashboards_dashboard-1.js') }}"></script>
-
-    <link href="{{ cAsset('assets/css/slides.css') }}" rel="stylesheet" type="text/css">
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script type="text/javascript" src="{{ cAsset('assets/css/koala.min.1.5.js') }}"></script>
 
@@ -66,139 +61,13 @@
     </style>
     <div class="main-content">
         <div class="page-content">
-            <div class="row">
-                <div class="col-lg-9">
             <div class="row" style="padding-top: 12px;">
                 <div class="col-lg-2">
                     <div class="row">
-                        <div class="col-md-12">
-                            <div class="card mb-4">                                
-                                <div class="card-body">
-                                    <div id="fsD1" class="focus" style="margin-left:0px">  
-                                        <div id="D1pic1" class="fPic">  
-                                            <div class="fcon" style="display: none;">
-                                                <a target="_blank" href="http://www.cnshipnet.com/news/14/67739.html"><img src="{{ cAsset('assets/img/1.jpg') }}" alt="江龙船艇30米级钢铝交通船试航" style="opacity: 1;"></a>
-                                                <span class="shadow"><h3><a target="_blank" href="http://www.cnshipnet.com/news/14/67739.html">江龙船艇30米级钢铝交通船试航</a></h3></span>
-                                            </div><div class="fcon" style="display: none;">
-                                                <a target="_blank" href="http://www.cnshipnet.com/news/14/67193.html"><img src="{{ cAsset('assets/img/2.jpg') }}" alt="启东中远海运海工风电安装船按计划完工" style="opacity: 1;"></a>
-                                                <span class="shadow"><h3><a target="_blank" href="http://www.cnshipnet.com/news/14/67193.html">启东中远海运海工风电安装船按计划完工</a></h3></span>
-                                            </div><div class="fcon" style="display: none;">
-                                                <a target="_blank" href="http://www.cnshipnet.com/news/14/67089.html"><img src="{{ cAsset('assets/img/3.jpg') }}" alt="重庆中江船业130米集散货船顺利开航离港" style="opacity: 1;"></a>
-                                                <span class="shadow"><h3><a target="_blank" href="http://www.cnshipnet.com/news/14/67089.html">重庆中江船业130米集散货船顺利开航离港</a></h3></span>
-                                            </div><div class="fcon" style="display: block;">
-                                                <a target="_blank" href="http://www.cnshipnet.com/news/14/66776.html"><img src="{{ cAsset('assets/img/4.jpg') }}" alt="达门新推双体深绞吸式挖泥船DCSD500" style="opacity: 0.91;"></a>
-                                                <span class="shadow"><h3><a target="_blank" href="http://www.cnshipnet.com/news/14/66776.html">达门新推双体深绞吸式挖泥船DCSD500</a></h3></span>
-                                            </div>    
-                                        </div>
-                                        <div class="fbg">  
-                                        <div class="D1fBt" id="D1fBt">  
-                                            <a href="javascript:void(0)" hidefocus="true" target="_self" class=""><i>1</i></a>  
-                                            <a href="javascript:void(0)" hidefocus="true" target="_self" class=""><i>2</i></a>  
-                                            <a href="javascript:void(0)" hidefocus="true" target="_self" class=""><i>3</i></a>  
-                                            <a href="javascript:void(0)" hidefocus="true" target="_self" class="current"><i>4</i></a>  
-                                        </div>  
-                                        </div>  
-                                        <span class="prev" style="opacity: 0.3;"></span>   
-                                        <span class="next" style="opacity: 0.3;"></span>    
-                                    </div>
                         <div class="card mb-4">
                             <div class="card-header decide-title">
                                 <div class="card-title front-span">
                                     <span class="bigger-120">等待批准</span>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                </div>
-                                
-                                <div class="card-body">
-                                    <div class="advertise">
-                                        <div style="width: 10%; padding-left: 16px;">
-                                            <h5 style="font-weight: bold;">重要公告 : </h5>
-                                        </div>
-                                        <div class="sign_list slider" style="width: 85%;">
-                                            @if(isset($reportList) && count($reportList) > 0)
-                                                @foreach ($reportList as $item)
-                                                    <div style="height: auto; outline: unset;">
-                                                        <h5>
-                                                            <a href="/decision/receivedReport?reportId={{$item->id}}" style="color: white; outline: unset;" target="_blank">
-                                                                从[{{ $item->realname }}]收到了[{{ $shipForDecision[$item->shipNo] }}]号的{{ g_enum('ReportTypeData')[$item->flowid][0] }}审批文件.
-                                                            </a>
-                                                        </h5>
-                                                    </div>
-                                                @endforeach
-                                            @else
-                                                <span>{{ trans('home.message.no_data') }}</span>
-                                            @endif
-                                        </div>
-                                        <div class="text-right" style="width: 5%; padding-right: 16px;">
-                                            <a href="/decision/receivedReport" style="color: white; text-decoration: underline;" target="_blank">更多</a>
-                                        </div>
                                 </div>
                                 <a href="#" class="more-btn">更多</a>
                             </div>
@@ -291,7 +160,6 @@
                                 </div>
                             </div>
 
-
                             <div class="card-body p-0" style="box-shadow: 0px 0px 8px 4px #d2d2d2;">
                                 <div class="advertise">
                                     <div style="padding-left: 16px;">
@@ -324,37 +192,6 @@
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div style="height: 210px; width: 100%;" class="mb-4">
-                                        <canvas id="statistics-chart-1"></canvas>
-                                    </div>
-                                    <div class="w-100" style="height: 200px;">
-                                        <canvas id="statistics-chart-3"></canvas>
-                                    </div>
-                                    <div class="table-responsive" id="decidemanage_list_table">
-                                        <table class="table table-striped table-bordered table-hover">
-                                            <thead>
-                                            <tr class="black br-hblue">
-                                                <th style="width:5%">{{ trans('home.table.no') }}</th>
-                                                <th style="width: 30%">{{ trans('home.table.ship_name') }}</th>
-                                                <th style="width:10%">{{ trans('home.table.ship_member_cnt') }}</th>
-                                                <th style="width: 20%">{{ trans('home.title.profit') }}</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            @if(isset($shipList) && count($shipList) > 0)
-                                                @foreach ($shipList as $key => $item)
-                                                    <tr>
-                                                        <td>{{ $key + 1 }}</td>
-                                                        <td>{{ $item->shipName_En }}({{ $item->shipName_Cn }})</td>
-                                                        <td>{{ $item->userCount }}</td>
-                                                        <td>{{ $key * 133 }}</td>
-                                                    </tr>
-                                                @endforeach
-                                            @else
                     <div class="row default-style main-panel">
                         <div class="card">
                             <div class="card-body">
@@ -454,16 +291,11 @@
                                         @if(isset($shipList) && count($shipList) > 0)
                                             @foreach ($shipList as $key => $item)
                                                 <tr>
-                                                    <td colspan="8">{{ trans('home.message.no_data') }}</td>
                                                     <td>{{ $key + 1 }}</td>
                                                     <td>{{ $item->shipName_En }}({{ $item->shipName_Cn }})</td>
                                                     <td>{{ $item->userCount }}</td>
                                                     <td>{{ $key * 133 }}</td>
                                                 </tr>
-                                            @endif
-                                            </tbody>
-                                        </table>
-                                    </div>
                                             @endforeach
                                         @else
                                             <tr>
@@ -473,24 +305,17 @@
                                         </tbody>
                                     </table>
                                 </div>
-                                
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3">
                 <div class="col-lg-2">
                     <div class="card">
-                        <div class="card-header">
-                            <div class="card-title">
-                                <span class="bigger-150">{{ trans('home.title.notice') }}</span>
                         <div class="card-header caption-title">
                             <div class="card-title front-span">
                                 <span class="bigger-120">货币汇率</span>
                             </div>
                         </div>
-                        <div class="card-body">
-                            {{ trans('home.message.no_data') }}
                         <div class="caption-border">
                             <table id="currency-list" v-cloak>
                                 <tbody>
@@ -518,7 +343,6 @@
             },
         });
 
-    <script>
         getCurrencyRate();
         setInterval(getCurrencyRate, 3000);
         function getCurrencyRate() {
@@ -546,38 +370,6 @@
             slidesToScroll: 1
         });
 
-        $(function() {
-            var chart1 = new Chart(document.getElementById('statistics-chart-1').getContext("2d"), {
-                type: 'line',
-                data: {
-                    labels: ['2020-08', '2020-09', '2020-10', '2020-11', '2020-12', '2021-01', '2021-02', '2021-03'],
-                    datasets: [{
-                        label: '收益',
-                        data: [93, 25, 95, 59, 46, 68, 4, 41],
-                        borderWidth: 1,
-                        backgroundColor: 'rgba(28,180,255,.05)',
-                        borderColor: 'rgba(28,180,255,1)'
-                    }],
-                },
-                options: {
-                    scales: {
-                        xAxes: [{
-                            gridLines: {
-                                display: false
-                            },
-                            ticks: {
-                                fontColor: '#aaa'
-                            }
-                        }],
-                        yAxes: [{
-                            gridLines: {
-                                display: false
-                            },
-                            ticks: {
-                                fontColor: '#aaa'
-                            }
-                        }]
-                    },
     </script>
     <script type="text/javascript">
         Qfast.add('widgets', { path: "{{ cAsset('assets/css/terminator.js') }}", type: "js", requires: ['fx'] });
@@ -597,10 +389,6 @@
             })
         });
 
-                    responsive: true,
-                    maintainAspectRatio: false
-                }
-            });
         c3.generate({
             bindto: '#c3-graph',
             color: { pattern: [ '#00BCD4', '#607D8B', 'red', '#f7931a' ] },
@@ -620,20 +408,6 @@
             }
         });
 
-            var chart3 = new Chart(document.getElementById('statistics-chart-3').getContext("2d"), {
-                type: 'bar',
-                data: {
-                    datasets: [{
-                        data: [24, 92, 77, 90, 91, 78, 28, 49, 23, 81],
-                        borderWidth: 1,
-                        barPercentage: 0.5,
-                        barThickness: 6,
-                        maxBarThickness: 8,
-                        minBarLength: 2,
-                        backgroundColor: '#82ccff',
-                    }],
-                    labels: ['船舶1', '船舶2', '船舶3', '船舶4', '船舶5', '船舶6', '船舶7', '船舶8', '船舶9', '船舶10']
-                },
         new Chartist.Bar('#chartist-h-bars', {
             labels: ['', ''],
             series: [
@@ -648,26 +422,6 @@
             axisY:             { offset: 10 }
         });
 
-                options: {
-                    scales: {
-                        xAxes: [{
-                            barPercentage: 0.5,
-                            barThickness: 30,
-                            maxBarThickness: 30,
-                            minBarLength: 2,
-                            display: true,
-                        }],
-                        yAxes: [{
-                            display: false
-                        }]
-                    },
-                    legend: {
-                        display: false
-                    },
-                    responsive: true,
-                    maintainAspectRatio: false
-                }
-            });
         new Chartist.Bar('#chartist-h-bars-02', {
             labels: ['', '', '', ''],
             series: [
@@ -682,24 +436,4 @@
             axisY:             { offset: 10 }
         });
     </script>
-    <script type="text/javascript">
-        Qfast.add('widgets', { path: "{{ cAsset('assets/css/terminator.js') }}", type: "js", requires: ['fx'] });  
-        Qfast(false, 'widgets', function () {
-        K.tabs({
-        id: 'fsD1',   //焦点图包裹id  
-        conId: "D1pic1",  //** 大图域包裹id  
-        tabId:"D1fBt",  
-        tabTn:"a",
-        conCn: '.fcon', //** 大图域配置class       
-        auto: 1,   //自动播放 1或0
-        effect: 'fade',   //效果配置
-        eType: 'click', //** 鼠标事件
-        pageBt:true,//是否有按钮切换页码
-        bns: ['.prev', '.next'],//** 前后按钮配置class                          
-        interval: 3000  //** 停顿时间  
-        }) 
-        }) 
-    </script>
-
-
 @endsection
